00686544cafb4ef8598bd054c9f8abf2afc6cbd0
yukesh570 <yukesh.maha10@gmail.com>
2025-11-20 15:55:42 +0545

    changes

--- next.config.ts
+++ next.config.ts
@@ -3,23 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        // protocol: "https",
-        // hostname: "dudu-backend.vercel.app",
-
-          protocol: "http",          // your backend is http
-        hostname: "192.168.1.151", // remove the port
-        port: "3000",              // specify port separately
-        pathname: "/**",      
-      },
         {
         protocol: "https",
         hostname: "backend.dudusoftware.com",
-        port: "", 
         pathname: "/**",
       },
     ],
-     unoptimized: false,
   },
 };
 
